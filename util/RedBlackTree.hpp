ace47e077906675521fe71a7738de55398e93c53
Anniball <lucas.delmas.an123@gmail.com>
2022-07-13 10:53:25 +0200

    added some construct for rbtree + get_uncle for rbnode

--- util/RedBlackTree.hpp
+++ util/RedBlackTree.hpp
@@ -6,7 +6,7 @@
 /*   By: ldelmas <ldelmas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 16:02:21 by ldelmas           #+#    #+#             */
-/*   Updated: 2022/07/12 18:00:19 by ldelmas          ###   ########.fr       */
+/*   Updated: 2022/07/13 10:45:24 by ldelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ namespace ft
 			pointer			get_right(void) const;
 			pointer			get_parent(void) const;
 			pointer			get_grand_parent(void) const;
+			pointer			get_uncle(void) const;
 			bool			is_red(void) const;
 			bool			is_black(void) const;
 
@@ -95,7 +96,6 @@ namespace ft
 	/*
 		GETTERS
 	*/
-	
 	template <class Key, class T, class Compare, class Alloc>
 	typename red_black_node<Key, T, Compare, Alloc>::key_type		&red_black_node<Key, T, Compare, Alloc>::get_key(void) const {return this->_content.first;}
 	
@@ -118,6 +118,19 @@ namespace ft
 	typename red_black_node<Key, T, Compare, Alloc>::pointer		red_black_node<Key, T, Compare, Alloc>::get_grand_parent(void) const {return this->_parent->_parent;}
 	
 	template <class Key, class T, class Compare, class Alloc>
+	typename red_black_node<Key, T, Compare, Alloc>::pointer		red_black_node<Key, T, Compare, Alloc>::get_uncle(void) const
+	{
+		pointer	parent = this->_parent;
+		pointer	grand_parent = parent->_parent;
+		if (!grand_parent)
+			return nullptr;
+		else if (grand_parent->_left == parent)
+			return grand_parent->_right;
+		else
+			return grand_parent->_left;
+	}
+
+	template <class Key, class T, class Compare, class Alloc>
 	bool															red_black_node<Key, T, Compare, Alloc>::is_red(void) const {return this->_isRed;}
 	
 	template <class Key, class T, class Compare, class Alloc>
@@ -128,6 +141,8 @@ namespace ft
 		RELATIONAL OPERATORS
 	*/
 
+	
+
 	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<pair<const Key,T> > >
 	class red_black_tree
 	{
@@ -135,6 +150,7 @@ namespace ft
 		typedef T													mapped_type;
 		typedef ft::pair<const Key, T>								value_type;
 		typedef ft::red_black_node<Key, T, Compare, Alloc>			node_type;
+		typedef ft::red_black_tree<Key, T, Compare, Alloc>			tree_type;
 		typedef Compare												key_compare;
 		typedef Alloc												allocator_type;
 		typedef	typename allocator_type::reference					reference;
@@ -148,112 +164,144 @@ namespace ft
 		typedef typename iterator_traits<iterator>::difference_type	difference_type;
 		typedef size_t												size_type;
 
+		public :
+			/*
+				CONSTRUCTORS AND DESTRUCTORS
+			*/
+			red_black_tree();
+			red_black_tree(const value_type &val);
+			red_black_tree(const node_type &val);
+			red_black_tree(const tree_type &src);
+			~red_black_tree();
+		
 		private :
-			
-		/*
-			---RULES---
-			balanced binary tree rules :
-				-each node can have until 2 subtrees :
-					-left items are smaller
-					-right items are larger
-			red black tree rules :
-			  	-a node is black or red
-				-NIL are black
-				-if a node is red its children are black
-				-all path from a node to leaves contain same amount of black nodes
-			red black tree subrules
-				-nodes need one storage bit to keep track of color
-				-longest path from root in no more twice the length of sorthest path
-					-direct consequence : shortest path is all black nodes and longest is alternating red and black
-		*/
-		/*
-			---SEARCH---
-			Just iterate or :
-			First rule: The first node in the tree is the leftmost node in the tree.
-			Next rule: The successor of a node is:
-				Next-R rule: If it has a right subtree, the leftmost node in the right subtree.
-				Next-U rule: Otherwise, traverse up the tree
-					If you make a right turn (i.e. this node was a left child), then that parent node is the successor
-					If you make a left turn (i.e. this node was a right child), continue going up.
-					If you can't go up anymore, then there's no successor
-			https://stackoverflow.com/questions/2942517/how-do-i-iterate-over-binary-tree
-		*/
-		/*
-			---ROTATIONS---
-			-Rearrange subtrees (not a simple "rotation")
-			-Goal is to decrease height of the tree
-				-larger subtrees up, smaller subtrees down
-			-Does not affect the order of elements
-
-			Left Rotate Pseudo-Code :
-			y = x.right
-			x.right = y.left
-			if y.left != T.nil
-				y.left.p = x
-			y.p = x.p
-			if x.p == T.nil
-				T.root = y
-			else if x == x.p.left
-				x.p.left = y
-			else
-				x.p.right = y
-			y.left = x
-			x.p = y
-		*/
-		/*
-			---INSERT---
-			Steps :
-				-Insert Z and color it red
-				-Recolor and rotate nodes to fix violation
-			Scenarios to fix violation (where Z is the node posing problem):
-				-Z = root -> color black
-				-Z.uncle = red -> recolor
-				-Z.uncle = black (triangle) -> rotate Z.parent //in opposite direction of Z I think?
-				-Z.uncle = black (line) -> rotate Z.grandparent + recolor //in opposite direction of Z I think?
-			Personal Note : A violation fixing can cause another violation, loop over the violation fixing while changing Z each time! 
-
-			Simple Insert Pseudo-Code :
-			y = T.nil
-			x = T.root
-			while x != T.nil
-				y = x
-				if z.key < x.key
-					x = x.left
-				else
-					x = x.right
-			z.p = y
-			if y == T.nil
-				T.root = z
-			else if z.key < y.key
-				y.left = z
-			else
-				y.right = z
-			z.left = T.nil
-			z.right = T.nil
-			z.color = RED
-			RB-INSERT-FIXUP(z) (Next Pseudo-Code)
-
-			Case 4 Fix Violation Scenario Pseudo-Code
-			while z.p.color == RED
-				if z.p == z.p.p.left
-					y = z.p.p.right
-					if y.color == RED
-						z.p.color = BLACK
-						y.color = BLACK
-						z.p.p.color = RED
-						z = z.p.p
-					else if z == z.p.right
-						z = z.p
-						LEFT-ROTATE(z)
-					z.p.color = BLACK
-					z.p.p.color = RED
-					RIGHT-ROTATE(z.p.p)
-				else (same but echange left and right)
-			T.root.color = BLACK (Case 0)
-			http://staff.ustc.edu.cn/~csli/graduate/algorithms/book6/chap14.htm
-		*/
+			node_type	_root;
 	};
+	
+	
+	/*
+		CONSTRUCTORS AND DESTRUCTORS
+	*/
+	template <class Key, class T, class Compare, class Alloc>
+	red_black_tree<Key, T, Compare, Alloc>::red_black_tree() : _root() {}
+	
+	template <class Key, class T, class Compare, class Alloc>
+	red_black_tree<Key, T, Compare, Alloc>::red_black_tree(const value_type &val) : _root(val) {}
+	
+	template <class Key, class T, class Compare, class Alloc>
+	red_black_tree<Key, T, Compare, Alloc>::red_black_tree(const node_type &node) : _root(node) {}
+	
+	// template <class Key, class T, class Compare, class Alloc>
+	// red_black_tree(const &tree_type src) {} //LET'S GO BACK ON IT LATER
+	
+	// template <class Key, class T, class Compare, class Alloc>
+	// ~red_black_tree() {} //LET'S GO BACK ON IT LATER
 }
+
+
+/*
+	---RULES---
+	balanced binary tree rules :
+		-each node can have until 2 subtrees :
+			-left items are smaller
+			-right items are larger
+	red black tree rules :
+		-a node is black or red
+		-NIL are black
+		-if a node is red its children are black
+		-all path from a node to leaves contain same amount of black nodes
+	red black tree subrules
+		-nodes need one storage bit to keep track of color
+		-longest path from root in no more twice the length of sorthest path
+			-direct consequence : shortest path is all black nodes and longest is alternating red and black
+*/
+/*
+	---SEARCH---
+	Just iterate or :
+	First rule: The first node in the tree is the leftmost node in the tree.
+	Next rule: The successor of a node is:
+		Next-R rule: If it has a right subtree, the leftmost node in the right subtree.
+		Next-U rule: Otherwise, traverse up the tree
+			If you make a right turn (i.e. this node was a left child), then that parent node is the successor
+			If you make a left turn (i.e. this node was a right child), continue going up.
+			If you can't go up anymore, then there's no successor
+
+	Iterate in order : https://stackoverflow.com/questions/2942517/how-do-i-iterate-over-binary-tree
+*/
+/*
+	---ROTATIONS---
+	-Rearrange subtrees (not a simple "rotation")
+	-Goal is to decrease height of the tree
+		-larger subtrees up, smaller subtrees down
+	-Does not affect the order of elements
+
+	Left Rotate Pseudo-Code :
+	y = x.right
+	x.right = y.left
+	if y.left != T.nil
+		y.left.p = x
+	y.p = x.p
+	if x.p == T.nil
+		T.root = y
+	else if x == x.p.left
+		x.p.left = y
+	else
+		x.p.right = y
+	y.left = x
+	x.p = y
+*/
+/*
+	---INSERT---
+	Steps :
+		-Insert Z and color it red
+		-Recolor and rotate nodes to fix violation
+	Scenarios to fix violation (where Z is the node posing problem):
+		-Z = root -> color black
+		-Z.uncle = red -> recolor
+		-Z.uncle = black (triangle) -> rotate Z.parent //in opposite direction of Z I think?
+		-Z.uncle = black (line) -> rotate Z.grandparent + recolor //in opposite direction of Z I think?
+	Personal Note : A violation fixing can cause another violation, loop over the violation fixing while changing Z each time! 
+
+	Simple Insert Pseudo-Code :
+	y = T.nil
+	x = T.root
+	while x != T.nil
+		y = x
+		if z.key < x.key
+			x = x.left
+		else
+			x = x.right
+	z.p = y
+	if y == T.nil
+		T.root = z
+	else if z.key < y.key
+		y.left = z
+	else
+		y.right = z
+	z.left = T.nil
+	z.right = T.nil
+	z.color = RED
+	RB-INSERT-FIXUP(z) (Next Pseudo-Code)
+
+	Case 4 Fix Violation Scenario Pseudo-Code
+	while z.p.color == RED
+		if z.p == z.p.p.left
+			y = z.p.p.right
+			if y.color == RED
+				z.p.color = BLACK
+				y.color = BLACK
+				z.p.p.color = RED
+				z = z.p.p
+			else if z == z.p.right
+				z = z.p
+				LEFT-ROTATE(z)
+			z.p.color = BLACK
+			z.p.p.color = RED
+			RIGHT-ROTATE(z.p.p)
+		else (same but echange left and right)
+	T.root.color = BLACK (Case 0)
+	http://staff.ustc.edu.cn/~csli/graduate/algorithms/book6/chap14.htm
+*/
 
 
 #endif
