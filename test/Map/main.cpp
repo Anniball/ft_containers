87a2b87630079b00cf7a7932bbe443b9f722281f
Anniball <lucas.delmas.an123@gmail.com>
2022-09-05 11:51:56 +0200

    improved tester + more test for ft::map

--- test/Map/main.cpp
+++ test/Map/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldelmas <ldelmas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 14:29:59 by ldelmas           #+#    #+#             */
-/*   Updated: 2022/09/02 15:08:59 by ldelmas          ###   ########.fr       */
+/*   Updated: 2022/09/05 11:50:27 by ldelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,118 @@
 
 t_timeval current_time;
 
+/*
+			map	&operator=(const map &x);
+			iterator							begin(void);
+			const_iterator						begin(void) const;
+			iterator							end(void);
+			const_iterator						end(void) const;
+			reverse_iterator					rbegin(void);
+			const_reverse_iterator				rbegin(void) const;
+			reverse_iterator					rend(void);
+			const_reverse_iterator				rend(void) const;
+			bool								empty(void) const;
+			size_type							size(void) const;
+			size_type							max_size(void) const;
+			mapped_type							&operator[](const key_type& k);
+			void								swap(map& x);
+			void								clear(void);
+			key_compare							key_comp(void) const;
+			value_compare						value_comp(void) const;
+			iterator							find(const key_type& k);
+			const_iterator						find(const key_type& k) const;
+			size_type							count(const key_type& k) const;
+			iterator							lower_bound(const key_type& k);
+			const_iterator						lower_bound(const key_type& k) const;
+			iterator							upper_bound(const key_type& k);
+			const_iterator						upper_bound(const key_type& k) const;
+			pair<const_iterator,const_iterator>	equal_range(const key_type& k) const;
+			pair<iterator,iterator>				equal_range(const key_type& k);
+*/
+
+static int test_construct(void)
+{
+	std::string filename = std::string(FILE_NAME) + "_construct_map";
+	freopen(filename.c_str(), "w", stdout);
+
+	initialize__time();
+	int_map m;
+	for (int i = 0; i < 100000; i++)
+		m.insert(int_pair(rand() % 100000, rand() % 10000));
+	int_map m2(m.begin(), m.end());
+	int_map m3(m2);
+	print_delta_time();
+
+	m.clear();
+	int_map::iterator itb = m.begin();
+	for (; itb != m.end(); ++itb)
+		std::cout << itb->first << " "  << itb->second << std::endl;
+	itb = m2.begin();
+	for (; itb != m2.end(); itb++)
+		std::cout << itb->first << " "  << itb->second << std::endl;
+	itb = m3.begin();
+	for (; itb != m3.end(); itb++)
+		std::cout << itb->first << " "  << itb->second << std::endl;
+	return 0;
+}
+
+
 static int	test_insert(void)
 {
 	std::string filename = std::string(FILE_NAME) + "_insert_map";
 	freopen(filename.c_str(), "w", stdout);
 
-	int_map m;
+	int_map m, m2;
+
 	initialize__time();
 	for (int i = 0; i < 100000; i++)
 		m.insert(int_pair(rand() % 100000, rand() % 10000));
+	m2.insert(m.begin(), m.end());
 	print_delta_time();
 	
 	int_map::iterator it = m.begin();
 	int_map::iterator ite = m.end();
 	for (; it != ite; it++)
 		std::cout << it->first << " " << it->second << std::endl;
+	return 0;
+}
+
+static int test_erase(void)
+{
+	std::string filename = std::string(FILE_NAME) + "_erase_map";
+	freopen(filename.c_str(), "w", stdout);
+
+	int_map m, m2, m3;
+	for (int i = 0; i < 10; i++)
+		m.insert(int_pair(rand() % 100, rand() % 10000));
+	m2 = m;
+	m3 = m;
+
+	int_map::iterator itb = m.end();
+	int_map::iterator itb2, itb3;
+	while (itb == m.end())
+		itb = m.find(rand() % 100);
+	
+	initialize__time();
+	while (itb != m.end())
+	{
+		int_map::iterator tmp = itb;
+		++itb;
+		m.erase(tmp);
+	}
+	for (size_t i = 0; i < m2.size(); i++)
+		m2.erase(rand() % 100);
+	m3.erase(m3.begin(), m3.end());	
+	print_delta_time();
+
+	itb = m.begin();
+	itb2 = m2.begin();
+	itb3 = m3.begin();
+	for (; itb != m.end(); itb++)
+		std::cout << itb->first << " " << itb->second << std::endl;
+	for (; itb2 != m2.end(); itb2++)
+		std::cout << itb2->first << " " << itb2->second << std::endl;
+		std::cout << m3.size() << std::endl;
 	return 0;
 }
 
@@ -40,6 +137,8 @@ int main(int ac, char **av)
 		return 1;
 	}
 	tester_setup(av[1]);
+	test_construct();
 	test_insert();
+	test_erase();
 	return 0;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldelmas <ldelmas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 16:30:15 by ldelmas           #+#    #+#             */
-/*   Updated: 2022/09/02 15:06:09 by ldelmas          ###   ########.fr       */
+/*   Updated: 2022/09/05 11:50:50 by ldelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,16 @@ static int	execute_tester(std::string str, std::string seed)
 	int status = 0;
 	int pid = fork();
 	if (pid < 0)
+	{
+		std::cout << "Forking of the process failed. Your session is probably full." << std::endl;
 		return 1;
+	}
 	else if (!pid)
 		execv(env[0], (char**)env);
 	else
 		waitpid(pid, &status, 0);
+	if (status)
+		std::cerr << "CRASH on " << str << std::endl;
 	return status;
 }
 
@@ -60,25 +65,33 @@ static int	compare_files(std::string std, std::string ft, std::string method)
 	std::cout << def << method << "\t\t";
 	Color::Modifier red(Color::FG_RED);
 	Color::Modifier green(Color::FG_GREEN);
-	std::ifstream ifs_std(std);
-	std::ifstream ifs_ft(ft);
+	try
+	{
+		std::ifstream ifs_std(std);
+		std::ifstream ifs_ft(ft);
 
-	std::string std_time, ft_time, std_str, ft_str;
-	std::getline(ifs_std, std_time);
-	std::getline(ifs_ft, ft_time);
+		std::string std_time, ft_time, std_str, ft_str;
+		std::getline(ifs_std, std_time);
+		std::getline(ifs_ft, ft_time);
 
-	std_str = std::string(std::istreambuf_iterator<char>(ifs_std), std::istreambuf_iterator<char>());
-	ft_str = std::string(std::istreambuf_iterator<char>(ifs_ft), std::istreambuf_iterator<char>());
-	std_str = std_str.substr(std_str.find("\n"), std_str.size());
-	ft_str = ft_str.substr(ft_str.find("\n"), ft_str.size());
-	int cmp = std_str.compare(ft_str);
-	if (!cmp) std::cout << green << "OK\t\t";
-	else std::cout << red << "DIFF\t\t"; 
+		std_str = std::string(std::istreambuf_iterator<char>(ifs_std), std::istreambuf_iterator<char>());
+		ft_str = std::string(std::istreambuf_iterator<char>(ifs_ft), std::istreambuf_iterator<char>());
+		std_str = std_str.substr(std_str.find("\n"), std_str.size());
+		ft_str = ft_str.substr(ft_str.find("\n"), ft_str.size());
+		int cmp = std_str.compare(ft_str);
+		if (!cmp) std::cout << green << "OK\t\t";
+		else std::cout << red << "DIFF\t\t" << cmp; 
 
-	print_timing(ft_time, std_time);
-	remove(std.c_str());
-	remove(ft.c_str());
-	return 0;
+		print_timing(ft_time, std_time);
+		remove(std.c_str());
+		remove(ft.c_str());
+		return 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Error, wrong file or non existing." << std::endl;
+		return 1;
+	}
 }
 
 static void	print_global_timing(void)
@@ -104,29 +117,36 @@ int			main(void)
 	int seed = rand() % 1000000;
 
 	print_class_header("VECTOR");
-	if (execute_tester("./Vector/ft_vec_test", std::to_string(seed))) return 1;
-	if (execute_tester("./Vector/std_vec_test", std::to_string(seed))) return 1;
-	compare_files("std_construct_vector", "ft_construct_vector", "Constr");
-	compare_files("std_access_vector", "ft_access_vector", "Access");
-	compare_files("std_insert_vector", "ft_insert_vector", "Insert");
-	compare_files("std_erase_vector", "ft_erase_vector", "Erase");
-	compare_files("std_assign_vector", "ft_assign_vector", "Assign");
-	compare_files("std_iter_vector", "ft_iter_vector", "Iter");
-	compare_files("std_size_vector", "ft_size_vector", "Size");
-	print_global_timing();
+	if (!execute_tester("./Vector/ft_vec_test", std::to_string(seed))
+		&& !execute_tester("./Vector/std_vec_test", std::to_string(seed)))
+	{
+		compare_files("std_construct_vector", "ft_construct_vector", "Constr");
+		compare_files("std_access_vector", "ft_access_vector", "Access");
+		compare_files("std_insert_vector", "ft_insert_vector", "Insert");
+		compare_files("std_erase_vector", "ft_erase_vector", "Erase");
+		compare_files("std_assign_vector", "ft_assign_vector", "Assign");
+		compare_files("std_iter_vector", "ft_iter_vector", "Iter");
+		compare_files("std_size_vector", "ft_size_vector", "Size");
+		print_global_timing();
+	}
 
 	print_class_header("STACK");
-	if (execute_tester("./Stack/ft_stk_test", std::to_string(seed))) return 1;
-	if (execute_tester("./Stack/std_stk_test", std::to_string(seed))) return 1;
-	compare_files("std_insert_stack", "ft_insert_stack", "Insert");
-	compare_files("std_erase_stack", "ft_erase_stack", "Erase");
-	print_global_timing();
+	if (!execute_tester("./Stack/ft_stk_test", std::to_string(seed))
+		&& !execute_tester("./Stack/std_stk_test", std::to_string(seed)))
+	{
+		compare_files("std_insert_stack", "ft_insert_stack", "Insert");
+		compare_files("std_erase_stack", "ft_erase_stack", "Erase");
+		print_global_timing();
+	}
 
 	print_class_header("MAP");
-	if (execute_tester("./Map/ft_map_test", std::to_string(seed))) return 1;
-	if (execute_tester("./MAp/std_map_test", std::to_string(seed))) return 1;
-	compare_files("std_insert_map", "ft_insert_map", "Insert");
-	print_global_timing();
-	
+	if (!execute_tester("./Map/ft_map_test", std::to_string(seed))
+		&& !execute_tester("./Map/std_map_test", std::to_string(seed)))
+	{
+		compare_files("std_construct_map", "ft_construct_map", "Constr");
+		compare_files("std_insert_map", "ft_insert_map", "Insert");
+		compare_files("std_erase_map", "ft_erase_map", "Erase");
+		print_global_timing();
+	}
 	return 0;
 }
